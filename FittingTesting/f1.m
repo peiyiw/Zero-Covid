8a03d1cdff58628bf9494cf92f07ed9d534971e2
peiyiwu <58755529+peiyiw@users.noreply.github.com>
2022-07-23 12:28:11 +0800

    Add files via upload

--- FittingTesting/f1.m
+++ FittingTesting/f1.m
@@ -23,7 +23,6 @@ params1 = {
       {'A0',10,0,50}
       {'P0',10,0,50}
       {'I0',10,0,50}
-      {'Tau0',0.1,0,1}
     };
 
 %%
@@ -36,8 +35,6 @@ params1 = {
 model.S20 = [4];
 model.N0  = [1];
 
-City = 'Tonghua'
-
 %%
 % First generate an initial chain.
 options.nsimu = 400000;
@@ -49,21 +46,37 @@ options.nsimu = 200000;
 options.stats = 1;
 [results2, chain2, s2chain2] = mcmcrun(model,data,params1,options,results);
 
-save('results2.mat','results2');
-save('chain2.mat','chain2');
-save('s2chain2.mat','s2chain2');
-
 
 %%
 % Chain plots should reveal that the chain has converged and we can
 % % use the results for estimation and predictive inference.
+
+  
 figure
 mcmcplot(chain2,[],results2,'denspanel',2);
-saveas(gcf,strcat(City,'_Chain2_posterior_probability'),'epsc');
 
 figure
 mcmcplot(chain2,[],results2); %,'pairs'
-saveas(gcf,strcat(City,'_Chain2_MCMC'),'epsc');
+%%
+% Function |chainstats| calculates mean ans std from the chain and
+% estimates the Monte Carlfigure
+% the integrated autocorrelation time and |geweke| is a simple test
+% for a null hypothesis that the chain has converged.
+
+results2.sstype = 1; % needed for mcmcpred and sqrt transformation
+chainstats(chain2,results2) %statistic results of parameter estimation
+
+%%
+% In order to use the |mcmcpred| function we need
+% function |modelfun| with input arguments given as
+% |modelfun(xdata,theta)|. We construct this as an anonymous function.
+modelfun = @(d,th) f3(d(:,1),th,d);
+
+% We sample 1000 parameter realizations from |chain| and |s2chain|
+% and calculate the predictive plots.
+nsample = 1000;
+results2.sstype = 1;
+out = mcmcpred(results2,chain2,s2chain2,data.xdata,modelfun,nsample);%data.ydata-->data
 
 
 
